0c827f6b0c7a91e341a949f26ad6c4f116f3bf6f
Tung Phan <tphannguyenthanh@artfx.fr>
2021-03-14 23:36:48 +0100

    Pong Game
    Pong made on Unity with C#

---
+++ Pong/Assets/Scenes/Script/P2Goal.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class P2Goal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject _scoretext;
+    public GameObject Ball;
+    public Rigidbody2D rb;
+    public float speed;
+    private int _score = 0;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("trigerred,P1 WIns");
+        _score++;
+        _scoretext.gameObject.GetComponent<TextMeshProUGUI>().text = _score.ToString();
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Ball.GetComponent<Transform>().position = new Vector2(0, 0);
+    }
+
+}
---
+++ Pong/Assets/Scenes/Script/Paddle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public bool isPlayer1;
+    public float speed;
+    public Rigidbody2D rb;
+
+    private float movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlayer1)
+        {
+            movement = Input.GetAxisRaw("Vertical"); // get the input axis we duplicated from the input manager, without smoothing
+            Debug.Log("P1 moving");
+        }
+        else
+        {
+            movement = Input.GetAxisRaw("Vertical 2");
+            Debug.Log("P2 moving");
+        }
+        rb.velocity = new Vector2(0, movement * speed /* Time.deltaTime*/); // vector 2 moves with the x of rigidbody, which is locked, and the y of Movement*speed*deltatime
+        Debug.Log("ez gud");    // doesn't work with delta time?
+    }
+
+}
